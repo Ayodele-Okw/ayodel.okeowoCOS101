3bde7317100726031b92ce26a06aaa1a38e7fd04
Ayodele-Okw <ayodeleokeowo09@gmail.com>
2025-10-13 19:47:33 +0100

    (my 3rd.6 commit)

---
+++ week-3/project_6/target/debug/deps/project_6.d
@@ -0,0 +1,5 @@
+C:\Users\HP\ayodel.okeowoCOS101\week-3\project_6\target\debug\deps\project_6.d: src\main.rs
+
+C:\Users\HP\ayodel.okeowoCOS101\week-3\project_6\target\debug\deps\project_6.exe: src\main.rs
+
+src\main.rs:
---
+++ week-3/project_6/target/debug/project_6.d
@@ -0,0 +1,1 @@
+C:\Users\HP\ayodel.okeowoCOS101\week-3\project_6\target\debug\project_6.exe: C:\Users\HP\ayodel.okeowoCOS101\week-3\project_6\src\main.rs
