890d35c98d1d9fd768112a64b73baa8cf8391519
Ayodele-Okw <ayodeleokeowo09@gmail.com>
2025-10-20 15:27:16 +0100

    (my 4th.4 commit)

---
+++ week-4/project_4/target/debug/deps/project_4.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Documents\Comp sci\ayodel.okeowoCOS101\week-4\project_4\target\debug\deps\project_4.d: src\main.rs
+
+C:\Users\HP\Documents\Comp sci\ayodel.okeowoCOS101\week-4\project_4\target\debug\deps\project_4.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/project_4/target/debug/project_4.d
@@ -0,0 +1,1 @@
+C:\Users\HP\Documents\Comp\ sci\ayodel.okeowoCOS101\week-4\project_4\target\debug\project_4.exe: C:\Users\HP\Documents\Comp\ sci\ayodel.okeowoCOS101\week-4\project_4\src\main.rs
