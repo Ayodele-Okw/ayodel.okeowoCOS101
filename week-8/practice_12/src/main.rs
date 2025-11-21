0b1f2ede87fccedfb0281209b587d060c87a33ed
Ayodele-Okw <ayodeleokeowo09@gmail.com>
2025-11-21 17:43:36 +0100

    (my 8th commit practice12)

---
+++ week-8/practice_12/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // mutable array
+    let mut colors = ["red", "green", "yellow", "white"];
+    
+    println!("\nOriginal array = {:?}", colors);
+
+    // mutable slice
+    let sliced_colors = &mut colors[1..3];
+    
+    println!("First slice = {:?}", sliced_colors);
+
+    // change the value of the original slice at the first index (index 1 of the slice corresponds to index 2 of the array)
+    sliced_colors[1] = "purple";
+    
+    println!("Changed slice = {:?}", sliced_colors);
+}
