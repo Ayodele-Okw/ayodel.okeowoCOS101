73a8fad964c0622f0140232c619c1c8826c369ef
Ayodele-Okw <ayodeleokeowo09@gmail.com>
2025-11-21 17:35:30 +0100

    (my 8th commit practice10)

---
+++ week-8/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\HP\Documents\Comp\ sci\ayodel.okeowoCOS101\week-8\practice_10\target\debug\practice_10.exe: C:\Users\HP\Documents\Comp\ sci\ayodel.okeowoCOS101\week-8\practice_10\src\main.rs
